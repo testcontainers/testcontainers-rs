1701fb7bb8ce7001c385cfbdc78fb7b4a1435ef1
Thomas Eizinger <thomas@eizinger.io>
2021-04-30 17:02:31 +1000

    Move `Docker` trait into a better place
    The `Docker` and `DockerAsync` trait are necessary for the `Container`
    and `ContainerAsync` structs to work properly. As such, they should
    be defined in there because those structs motivate their existence.

--- src/core.rs
+++ src/core.rs
@@ -1,18 +1,18 @@
-pub(crate) use self::docker::Docker;
+pub(crate) use container::Docker;
+pub(crate) use container_async::DockerAsync;
+
 pub use self::{
     container::Container,
     container_async::ContainerAsync,
-    docker::RunArgs,
     image::{Image, Port, WaitFor},
 };
+pub use run_args::RunArgs;
 
 mod container;
 mod container_async;
-mod docker;
 pub mod env;
 mod image;
+mod run_args;
 
 pub(crate) mod logs;
 pub(crate) mod ports;
-
-pub(crate) use container_async::DockerAsync;
