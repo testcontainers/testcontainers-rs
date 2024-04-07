4a0b1c4c5791174a3127f59d17b913d77a9b49d8
Artem Medvedev <i@ddtkey.com>
2024-04-07 22:03:03 +0200

    refactor: update usage in accordance with axum migration notes

--- testimages/src/bin/no_expose_port.rs
+++ testimages/src/bin/no_expose_port.rs
@@ -11,9 +11,9 @@ async fn main() {
 
     // run it
     let addr = SocketAddr::from(([0, 0, 0, 0], 8080));
+    let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
     println!("listening on {addr}");
-    axum::Server::bind(&addr)
-        .serve(app.into_make_service())
+    axum::serve(listener, app.into_make_service())
         .with_graceful_shutdown(shutdown_signal())
         .await
         .unwrap();
