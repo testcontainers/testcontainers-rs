6cbdbaa31c7ae6649dc12802387dd3c9423363ae
Artem Medvedev <i@ddtkey.com>
2024-04-22 17:03:41 +0200

    fix

--- testcontainers/src/core/runners/mod.rs
+++
@@ -1,9 +0,0 @@
-pub(crate) mod async_runner;
-#[cfg(feature = "blocking")]
-pub(crate) mod sync_runner;
-
-pub use self::async_runner::AsyncRunner;
-
-#[cfg(feature = "blocking")]
-#[cfg_attr(docsrs, doc(cfg(feature = "blocking")))]
-pub use self::sync_runner::SyncRunner;
