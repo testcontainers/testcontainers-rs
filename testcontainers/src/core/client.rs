14370b2e561d4474e1f698045bb371617f3291e9
mbodmer <marc.bodmer@email.ch>
2025-06-25 00:24:45 +0200

    feat: add build_image() to client
    Signed-off-by: mbodmer <marc.bodmer@email.ch>

--- testcontainers/src/core/client.rs
+++ testcontainers/src/core/client.rs
@@ -23,9 +23,8 @@ use url::Url;
 
 use crate::core::{
     client::exec::ExecResult,
-    copy::{CopyToContainer, CopyToContainerError},
-    env,
-    env::ConfigurationError,
+    copy::{CopyToContainer, CopyToContainerCollection, CopyToContainerError},
+    env::{self, ConfigurationError},
     logs::{
         stream::{LogStream, RawLogStream},
         LogFrame, LogSource, WaitingStreamWrapper,
@@ -61,6 +60,11 @@ pub enum ClientError {
     InvalidDockerHost(String),
     #[error("failed to pull the image '{descriptor}', error: {err}")]
     PullImage {
+        descriptor: String,
+        err: BollardError,
+    },
+    #[error("failed to build the image '{descriptor}', error: {err}")]
+    BuildImage {
         descriptor: String,
         err: BollardError,
     },
@@ -365,6 +369,37 @@ impl Client {
             return Ok(None);
         }
         Ok(state.exit_code)
+    }
+
+    pub(crate) async fn build_image(
+        &self,
+        descriptor: &str,
+        build_context: &CopyToContainerCollection,
+    ) -> Result<(), ClientError> {
+        let tar = build_context
+            .tar()
+            .await
+            .map_err(ClientError::CopyToContainerError)?;
+
+        let options = bollard::image::BuildImageOptions {
+            dockerfile: "Dockerfile",
+            t: descriptor,
+            rm: true,
+            ..Default::default()
+        };
+
+        let credentials = None; // todo
+
+        let mut building = self.bollard.build_image(options, credentials, Some(tar));
+
+        while let Some(result) = building.next().await {
+            result.map_err(|err| ClientError::BuildImage {
+                descriptor: descriptor.into(),
+                err,
+            })?;
+        }
+
+        Ok(())
     }
 
     pub(crate) async fn pull_image(&self, descriptor: &str) -> Result<(), ClientError> {
