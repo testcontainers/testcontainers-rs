29b380ca9d4942271864fc582e4f0ba1dccc7abd
mbodmer <marc.bodmer@email.ch>
2025-06-26 23:27:53 +0200

    chore: satisfy rustfmt
    Signed-off-by: mbodmer <marc.bodmer@email.ch>

--- testcontainers/src/runners/mod.rs
+++ testcontainers/src/runners/mod.rs
@@ -1,17 +1,18 @@
 pub(crate) mod async_builder;
+pub(crate) mod async_runner;
+
+pub use self::{async_builder::AsyncBuilder, async_runner::AsyncRunner};
+
 #[cfg(feature = "blocking")]
 pub(crate) mod sync_builder;
 
-pub(crate) mod async_runner;
-#[cfg(feature = "blocking")]
-pub(crate) mod sync_runner;
-
-pub use self::async_builder::AsyncBuilder;
 #[cfg(feature = "blocking")]
 #[cfg_attr(docsrs, doc(cfg(feature = "blocking")))]
 pub use self::sync_builder::SyncBuilder;
 
-pub use self::async_runner::AsyncRunner;
+#[cfg(feature = "blocking")]
+pub(crate) mod sync_runner;
+
 #[cfg(feature = "blocking")]
 #[cfg_attr(docsrs, doc(cfg(feature = "blocking")))]
 pub use self::sync_runner::SyncRunner;
