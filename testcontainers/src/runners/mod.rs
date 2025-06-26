31f1efc0677fd9b2716fe0ae50ec27b8a4ad1c0d
mbodmer <marc.bodmer@email.ch>
2025-06-26 13:38:35 +0200

    feat: add SyncBuilder
    Signed-off-by: mbodmer <marc.bodmer@email.ch>

--- testcontainers/src/runners/mod.rs
+++ testcontainers/src/runners/mod.rs
@@ -1,10 +1,15 @@
 pub(crate) mod async_builder;
+#[cfg(feature = "blocking")]
+pub(crate) mod sync_builder;
 
 pub(crate) mod async_runner;
 #[cfg(feature = "blocking")]
 pub(crate) mod sync_runner;
 
 pub use self::async_builder::AsyncBuilder;
+#[cfg(feature = "blocking")]
+#[cfg_attr(docsrs, doc(cfg(feature = "blocking")))]
+pub use self::sync_builder::SyncBuilder;
 
 pub use self::async_runner::AsyncRunner;
 #[cfg(feature = "blocking")]
